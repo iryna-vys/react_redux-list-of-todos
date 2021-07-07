13c87148de4aece6e075f201248614e0b2d27fb3
iryna-vys <vys.iryna@gmail.com>
2021-07-07 15:14:13 +0300

    Updates

---
+++ precache-manifest.84423e7013d1c5db8f0ebad11a0532ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4156de67c4fcadab11c4fe9aeb2195",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b418d3a98c1eb6a48556",
+    "url": "./static/css/main.8cc5052a.chunk.css"
+  },
+  {
+    "revision": "b072fd8892adda4925aa",
+    "url": "./static/js/2.5de1e259.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.5de1e259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b418d3a98c1eb6a48556",
+    "url": "./static/js/main.27ee54e0.chunk.js"
+  },
+  {
+    "revision": "6e2c239f7a333241bc85",
+    "url": "./static/js/runtime-main.cb0ead06.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.15153b8f6d6ca4fa16790bf3db195840.js"
+  "./precache-manifest.84423e7013d1c5db8f0ebad11a0532ed.js"
 );
 
 self.addEventListener('message', (event) => {
